d91cae909d4d874411bf96c8c223690f0b9c6c61
KamilWuu <kamilptakiprzygody@gmail.com>
2024-11-12 19:42:05 +0100

    macierze transformacji2

--- code/main_code/globalCoordSys.hh
+++ code/main_code/globalCoordSys.hh
@@ -3,32 +3,31 @@
 
 #include "robot.h"
 
-typedef struct TransformMatrix
-{
-    double RotationMatrix_X[3][3];
-};
-
-
 
 typedef struct RobotRotationsAngles
 {
     double _rot_X;
     double _rot_Y;
     double _rot_Z;
-};
+};  
 
 typedef struct ArcCenterCoordSys
 {
-    PositionVector LegPositionsArcCenter[6];
-
+    Robot _robot;
+    PositionVector _legs_positions_arc[6];
+    PositionVector _robot_center_position_arc;
+    RobotRotationsAngles _robot_angles;
+    double _arc_radius;
+    double _arc_angle;
 };
 
 
 typedef struct GlobalCoordSys
 {
-    RobotRotationsAngles _robot_angles;
-    Robot _robot;
-
+    ArcCenterCoordSys _arc_center_coord_sys;
+    PositionVector _robot_center_position_global;
+    PositionVector _legs_positions_global[6];
+    PositionVector _arc_center_position_global;
 };
 
 
--- code/main_code/transformMatrix.hh
+++ code/main_code/transformMatrix.hh
@@ -1,21 +1,26 @@
 #ifndef TRANSFORMMATRIX_H
 #define TRANSFORMMATRIX_H
 
-
 #include <math.h>
 #include "kinematics.h"
-typedef struct 
+
+
+typedef struct PositionVector
 {
     double P_x;
     double P_y;
     double P_z;
 
-}PositionVector;
+};
 
+typedef struct RotXYZVector
+{
+    double _Rot_x;
+    double _Rot_y;
+    dobule _Rot_z;
+};
 
 typedef struct Matrix3{
-    int rows;
-    int columns;
     double data[3][3];
 };
 
@@ -54,19 +59,72 @@ Matrix3 createRotMatrix(double rot_X, double rot_Y, double rot_Z){
 }
 
 TransformMatrix createTransformMatrix(double rot_X, double rot_Y, double rot_Z, double t_x, double t_y, double t_z){
-    TransformMatrix transform_matrix;
+    TransformMatrix out_transform_matrix;
     
-    transform_matrix._rotationMatrix = createRotMatrix(rot_X, rot_Y, rot_Z);
-    transform_matrix._translationVector.P_x = t_x;
-    transform_matrix._translationVector.P_y = t_y; 
-    transform_matrix._translationVector.P_z = t_z;
+    out_transform_matrix._rotationMatrix = createRotMatrix(rot_X, rot_Y, rot_Z);
+    out_transform_matrix._translationVector.P_x = t_x;
+    out_transform_matrix._translationVector.P_y = t_y; 
+    out_transform_matrix._translationVector.P_z = t_z;
 
-    return transform_matrix;
+    return out_transform_matrix;
 }
 
-void initTransformMatrix(TransformMatrix * transform_matrix, double rot_X, double rot_Y, double rot_Z, double t_x, double t_y, double t_z){
+//wspolrzednie wzgledem srodka robota na wspolrzedne w ukladzie globalnym 
+PositionVector TransformVector(PositionVector input_vector, TransformMatrix transform_matrix) { 
+    PositionVector output_vector, temp_vector;
+    Matrix3 rot_matrix = transform_matrix._rotationMatrix;
+    PositionVector trans_vector = transform_matrix._translationVector;
 
+    // Zastosowanie rotacji
+    temp_vector.P_x = rot_matrix.data[0][0] * input_vector.P_x + rot_matrix.data[0][1] * input_vector.P_y + rot_matrix.data[0][2] * input_vector.P_z;
+    temp_vector.P_y = rot_matrix.data[1][0] * input_vector.P_x + rot_matrix.data[1][1] * input_vector.P_y + rot_matrix.data[1][2] * input_vector.P_z;
+    temp_vector.P_z = rot_matrix.data[2][0] * input_vector.P_x + rot_matrix.data[2][1] * input_vector.P_y + rot_matrix.data[2][2] * input_vector.P_z;
+
+    // Dodanie wektora translacji
+    output_vector.P_x = temp_vector.P_x + trans_vector.P_x;
+    output_vector.P_y = temp_vector.P_y + trans_vector.P_y;
+    output_vector.P_z = temp_vector.P_z + trans_vector.P_z;
+
+    return output_vector;
 }
 
+//wspolrzedne globalne na wspolrzedne wzgledem srodka robota
+PositionVector inverseTransformVector(PositionVector input_vector, TransformMatrix transform_matrix) {
+    PositionVector output_vector, temp_vector;
+    Matrix3 rot_matrix = transform_matrix._rotationMatrix;
+    PositionVector trans_vector = transform_matrix._translationVector;
+
+    // Transpozycja macierzy rotacji
+    Matrix3 rot_matrix_T;
+    rot_matrix_T.data[0][0] = rot_matrix.data[0][0];
+    rot_matrix_T.data[0][1] = rot_matrix.data[1][0];
+    rot_matrix_T.data[0][2] = rot_matrix.data[2][0];
+
+    rot_matrix_T.data[1][0] = rot_matrix.data[0][1];
+    rot_matrix_T.data[1][1] = rot_matrix.data[1][1];
+    rot_matrix_T.data[1][2] = rot_matrix.data[2][1];
+
+    rot_matrix_T.data[2][0] = rot_matrix.data[0][2];
+    rot_matrix_T.data[2][1] = rot_matrix.data[1][2];
+    rot_matrix_T.data[2][2] = rot_matrix.data[2][2];
+
+    // Przesunięcie translacji w odniesieniu do odwróconej rotacji
+    PositionVector trans_inverse;
+    trans_inverse.P_x = -(rot_matrix_T.data[0][0] * trans_vector.P_x + rot_matrix_T.data[0][1] * trans_vector.P_y + rot_matrix_T.data[0][2] * trans_vector.P_z);
+    trans_inverse.P_y = -(rot_matrix_T.data[1][0] * trans_vector.P_x + rot_matrix_T.data[1][1] * trans_vector.P_y + rot_matrix_T.data[1][2] * trans_vector.P_z);
+    trans_inverse.P_z = -(rot_matrix_T.data[2][0] * trans_vector.P_x + rot_matrix_T.data[2][1] * trans_vector.P_y + rot_matrix_T.data[2][2] * trans_vector.P_z);
+
+    // Zastosowanie odwróconej rotacji do punktu wejściowego
+    temp_vector.P_x = rot_matrix_T.data[0][0] * input_vector.P_x + rot_matrix_T.data[0][1] * input_vector.P_y + rot_matrix_T.data[0][2] * input_vector.P_z;
+    temp_vector.P_y = rot_matrix_T.data[1][0] * input_vector.P_x + rot_matrix_T.data[1][1] * input_vector.P_y + rot_matrix_T.data[1][2] * input_vector.P_z;
+    temp_vector.P_z = rot_matrix_T.data[2][0] * input_vector.P_x + rot_matrix_T.data[2][1] * input_vector.P_y + rot_matrix_T.data[2][2] * input_vector.P_z;
+
+    // Dodanie odwróconego wektora translacji
+    output_vector.P_x = temp_vector.P_x + trans_inverse.P_x;
+    output_vector.P_y = temp_vector.P_y + trans_inverse.P_y;
+    output_vector.P_z = temp_vector.P_z + trans_inverse.P_z;
+
+    return output_vector;
+}
 
 #endif //TRANSFORMMATRIX.H
